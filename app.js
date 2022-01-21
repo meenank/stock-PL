5a71f6d2dae0342366046c58bf977a9b89d3fb11
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-21 15:44:50 +0530

    completed wiring and res show part

---
+++ app.js
@@ -0,0 +1,35 @@
+const first_price = document.querySelector("#intial-price");
+const no_of_stocks = document.querySelector("#units");
+const last_price = document.querySelector("#current-price");
+const btn = document.querySelector("button");
+const res_area = document.querySelector(".result");
+
+function testInputs() {
+
+    console.log(first_price.value);
+    console.log(no_of_stocks.value);
+    console.log(last_price.value);
+
+}
+
+function showResult() {
+
+    const first_inv = first_price.value * no_of_stocks.value;
+    const cur_inv = last_price.value * no_of_stocks.value;
+    const diff_inv = cur_inv - first_inv;
+    // console.log("Diff = ", diff_inv);
+
+    var percentage_pl = (diff_inv / first_inv) * 100;
+    percentage_pl = percentage_pl.toFixed(2);
+    if (percentage_pl > 0) {
+        res_area.innerHTML = "you got a profit of +" + percentage_pl + "%...CONGRATS!!! 🥳";
+    } else if (percentage_pl < 0) {
+        res_area.innerHTML = "You got a loss of " + percentage_pl + "%...time to awake harshad mehta in you 😜";
+    } else {
+        res_area.innerHTML = "No Pain no Gain  but here...its no Gain so no Pain"
+    }
+
+}
+
+
+btn.addEventListener("click", showResult)
